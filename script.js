d76bc492d9a3d2b5e1eb86e72765ec0a62f5c5de
xjken <63294438+xjken@users.noreply.github.com>
2026-02-22 06:27:14 +0700

    added function to add books from library to html

--- script.js
+++ script.js
@@ -32,17 +32,38 @@ function showAllBookFromLibrary(library){
 } 
 
 // Some books to fill the array
-addBookToLibrary(myLibrary,'The Hobbit', 'J.R.R Tolkier', 295, 2003, false);
-addBookToLibrary(myLibrary, "1984", "George Orwell", 328, 1949, true);
-addBookToLibrary(myLibrary, "To Kill a Mockingbird", "Harper Lee", 281, 1960, true);
-addBookToLibrary(myLibrary, "Pride and Prejudice", "Jane Austen", 432, 1813, false);
-addBookToLibrary(myLibrary, "The Great Gatsby", "F. Scott Fitzgerald", 180, 1925, true);
-addBookToLibrary(myLibrary, "Dune", "Frank Herbert", 412, 1965, false);
-addBookToLibrary(myLibrary, "The Catcher in the Rye", "J.D. Salinger", 214, 1951, false);
-addBookToLibrary(myLibrary, "Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, 1997, true);
-addBookToLibrary(myLibrary, "The Alchemist", "Paulo Coelho", 208, 1988, false);
-addBookToLibrary(myLibrary, "The Little Prince", "Antoine de Saint-Exupéry", 96, 1943, true);
+{
+    addBookToLibrary(myLibrary,'The Hobbit', 'J.R.R Tolkien', 295, 2003, false);
+    addBookToLibrary(myLibrary, "1984", "George Orwell", 328, 1949, true);
+    addBookToLibrary(myLibrary, "To Kill a Mockingbird", "Harper Lee", 281, 1960, true);
+    addBookToLibrary(myLibrary, "Pride and Prejudice", "Jane Austen", 432, 1813, false);
+    addBookToLibrary(myLibrary, "The Great Gatsby", "F. Scott Fitzgerald", 180, 1925, true);
+    addBookToLibrary(myLibrary, "Dune", "Frank Herbert", 412, 1965, false);
+    addBookToLibrary(myLibrary, "The Catcher in the Rye", "J.D. Salinger", 214, 1951, false);
+    addBookToLibrary(myLibrary, "Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, 1997, true);
+    addBookToLibrary(myLibrary, "The Alchemist", "Paulo Coelho", 208, 1988, false);
+    addBookToLibrary(myLibrary, "The Little Prince", "Antoine de Saint-Exupéry", 96, 1943, true);
+}
 
 
-const bookContainer = document.querySelector(".book-container");
-// bookContainer.textContent=showAllBookFromLibrary(myLibrary);
+const booksContainer = document.querySelector(".books-container");
+
+const createBookCard = function(book){
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    bookCard.innerHTML = `
+        <p class="book-title">${book.title}</p>
+        <p class="book-author">${book.author}</p>
+        <p class="book-year">${book.yearPublished}</p>
+        <p class="book-pages">${book.pages}</p>
+        <button class="book-read">${book.isRead ? "read" : "not read yet"}</button>
+    `
+    return bookCard;
+}
+
+const addLibraryToContainer = function(){
+    for (const book of myLibrary){
+        booksContainer.appendChild(createBookCard(book))
+    }
+}
+addLibraryToContainer();
